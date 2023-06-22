b201d7ff15c3ee60fd12f540f2234ee0d74f7ce1
Stepan Skichko <st.skichko@gmail.com>
2023-06-22 14:13:32 +0300

    update

--- Firebase/config.js
+++ Firebase/config.js
@@ -2,13 +2,14 @@ import { initializeApp } from 'firebase/app';
 import { getAnalytics } from 'firebase/analytics';
 
 const firebaseConfig = {
-  apiKey: '',
-  authDomain: '',
-  projectId: '',
-  storageBucket: '',
-  messagingSenderId: '',
-  appId: '',
-  measurementId: '',
+  apiKey: 'AIzaSyAzpX4yEbPX7c_rAmMisiBU7N1iIAcRGpI',
+  authDomain: 'react-native-hw-d6aa0.firebaseapp.com',
+  databaseURL: 'https://react-native-hw-d6aa0-default-rtdb.europe-west1.firebasedatabase.app',
+  projectId: 'react-native-hw-d6aa0',
+  storageBucket: 'react-native-hw-d6aa0.appspot.com',
+  messagingSenderId: '967673438485',
+  appId: '1:967673438485:web:9c7b99687aec1eccf7c91c',
+  measurementId: 'G-14Y03EH1Z7',
 };
 
 const app = initializeApp(firebaseConfig);
